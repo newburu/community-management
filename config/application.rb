0abbfb67a436bf8bc408f6c2d5349576f19b103e
Newburu <newburu@gmail.com>
2024-04-27 21:41:16 +0900

    日本語ファイルを追加

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,13 @@ module CommunityManagement
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    # デフォルトのlocaleを日本語(:ja)にする
+    config.i18n.default_locale = :ja
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     config.generators.template_engine = :slim #slimに変更
   end

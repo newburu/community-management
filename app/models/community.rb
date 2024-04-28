0f49cd1f30bf3eb5f7fe1251fdf265ac38893318
Newburu <newburu@gmail.com>
2024-04-28 15:16:17 +0900

    コミュニティに画像（アイコン）を登録できるようにする Fixes #7

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,3 +1,9 @@
 class Community < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: :owner_id
+
+  mount_uploader :icon, ImageUploader
+
+  def random_image
+    self.icon
+  end
 end

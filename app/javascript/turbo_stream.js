ef070e2068f6e6bf16c6db8b9a5c847e152ca360
Newburu <newburu@gmail.com>
2024-05-19 15:31:15 +0900

    ユーザ編集後、参照画面(/show)に戻す Fixes #17

---
+++ app/javascript/turbo_stream.js
@@ -0,0 +1,21 @@
+document.addEventListener('turbo:before-fetch-response', (event) => {
+  var json = JSON.parse(
+    event.detail.fetchResponse.header('X-Flash-Messages')
+  )
+  // メッセージを表示する
+  for(const key in json){
+    alert(key + ':' + decodeURI(json[key]))
+  }
+  
+  // ---------------
+  // 追加
+  // ---------------
+  // turbo frameから リダイレクトを望まれていたらリダイレクトする
+  if(event.detail.fetchResponse.header('X-Turbo-Connect') && typeof (event.detail.fetchResponse) !== 'undefined') {
+    var response = event.detail.fetchResponse.response
+    if (response.redirected) {
+      event.preventDefault()
+      Turbo.visit(response.url, {action: 'replace'})
+    }
+  }
+})

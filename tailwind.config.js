8af03541090ef61f63b78e496e70781e77c3bf4b
にゅ〜ぶる <newburu@gmail.com>
2024-06-27 21:45:12 +0900

    自動デプロイ対応 (#21)
    * 自動デプロイ対応
    Fixes #20
    
    * daisyuiの導入方法を変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+      './app/views/**/*.html.slim',
+      './app/helpers/**/*.rb',
+      './app/assets/stylesheets/**/*.css',
+      './app/javascript/**/*.js'
   ],
   theme: {
     extend: {
